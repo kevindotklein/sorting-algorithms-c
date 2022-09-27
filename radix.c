a11835b82a8b9eadc96bdac6f4b49b99e4e1c518
kevindotklein <kevindotklein@gmail.com>
2022-09-26 22:54:04 -0300

    todo: radixsort

---
+++ radix.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "utils.h"
+
+#define SIZE 5
+
+void radix_sort(int *arr, int size);
+
+int main(void){
+  srand(time(NULL));
+  
+  return 0;
+}
+
+void radix_sort(int *arr, int size){
+
+}
