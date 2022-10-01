f1328f4f4b190ee905bd5f15161adf0a0c98fc46
kevindotklein <kevindotklein@gmail.com>
2022-09-30 22:18:39 -0300

    counting sort

---
+++ coutingsort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "utils.h"
+
+#define SIZE 5
+
+void coutingsort(int *arr, int size);
+
+int main(void){
+  srand(time(NULL));
+  return 0;
+}
+
+void coutingsort(int *arr, int size){
+  
+}
